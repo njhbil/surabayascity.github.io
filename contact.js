9654fa051619da3506d158e773de84502843cf9b
njhbil <24051204074@mhs.unesa.ac.id>
2024-10-09 00:40:53 +0700

    penyempurnaan

---
+++ contact.js
@@ -0,0 +1,11 @@
+let nama, email, keluhan, hasil;
+
+document.getElementById ('button').addEventListener (onclick=function() {
+nama = getElementById('name').value;
+email = getElementById ('email').value;
+keluhan = getElementById ('keluhan').value;
+
+console.log(nama);
+console.log(email);
+console.log(keluhan);
+})
